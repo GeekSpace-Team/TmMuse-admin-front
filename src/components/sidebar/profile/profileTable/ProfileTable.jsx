3b45fd55fe068a24fa9799a28b9540ffc95d042b
Halil777 <microsoft7779@gmail.com>
2022-03-09 17:25:28 +0500

    commit

--- src/components/sidebar/profile/profileTable/ProfileTable.jsx
+++ src/components/sidebar/profile/profileTable/ProfileTable.jsx
@@ -61,7 +61,7 @@ const ProfileTable = (props) => {
                 <th><center>ID</center></th>
                 <th><center>Name</center></th>
                 <th><center>Image</center></th>
-                <th><center>Category</center></th>
+                <th><center>category</center></th>
                 <th><center>vip status</center></th>
                 <th><center>status</center></th>
                 <th><center>View count</center></th>
